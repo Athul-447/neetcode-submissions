ba13da4f516af80d7161b28b372940c1eb9b120e
Athul <154679346+Athul-447@users.noreply.github.com>
2026-07-09 22:10:02 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            min = i
+            for j in range(i+1,(len(nums))):
+                if nums[j] < nums[min]:
+                    min = j
+            nums[i],nums[min] = nums[min],nums[i]
+        return nums
